f3a9a2f05f8ab9bbac63d981528c44393d4b999e
Rick Cobb <rec@grandroundshealth.com>
2013-11-15 11:27:08 -0800

    Write the linkup PWD finder correctly. Not tested.

--- rick/linkup.sh
+++ rick/linkup.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-LINKTOHERE=./src/cc/cc/user/rick
+# For more comprehensive solution, see
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for i in .?* ; do
     [ $i == '..' ] && continue
     [ $i == .ssh ] && continue
-    if [ ! -L ~/$i -a -e "$LINKTOHERE/$i" ] ; then
+    if [ ! -L ~/$i -a -e "$DIR/$i" ] ; then
         echo "Linking $i"
         mv ~/$i ~/$i.bak
-        ln -s $LINKTOHERE/$i ~/$i
+        ln -s $DIR/$i ~/$i
     fi
 done
