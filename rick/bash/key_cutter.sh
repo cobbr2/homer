c5d99268d818a06d7f2f90cb1b6679d56adec148
Rick Cobb <rick@grandrounds.com>
2016-10-25 10:42:52 -0700

    Not needed now that gr-replace-psk is sensible

--- rick/bash/key_cutter.sh
+++
@@ -1,8 +0,0 @@
-export IPSEC_SECRETS_FILE=/etc/ipsec.secrets
-
-function key_cutter() {
-  echo "You'll probably need two ^Ds" >/dev/tty
-  echo "\"$(gpg -o - -i -)\"" | clip
-  sudo vim /etc/ipsec.secrets
-  sudo ipsec restart
-}
