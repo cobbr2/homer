bad1769647a3293e5d6e7a2134483bd50788f34e
Rick Cobb <rick@grandrounds.com>
2019-01-11 12:11:23 -0800

    Replace ec2-find with tools that work from home.

--- rick/bash/aws.sh
+++ rick/bash/aws.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env sh
-# Can probably remove all of these:
-#export JAVA_HOME=/usr
-#export EC2_HOME=${HOME}/ec2
-#export PATH=${PATH}:${EC2_HOME}/bin
-#export EC2_CERT=${HOME}/.ec2/cert.pem
-#export EC2_PRIVATE_KEY=${HOME}/.ec2/pk.pem
-#export EC2_URL=https://ec2.us-east-1.amazonaws.com
 
 export AWS_ENVIRONMENT_BETA=1
 export AWS_DEFAULT_ROLE=developer
@@ -17,3 +10,25 @@ fi
 alias s3='aws s3'
 
 alias 'aws-refresh'='aws-environment "${AWS_ENVIRONMENT}"'
+
+ec2-ip() {
+  field=3
+  usage='ec2-ip [ --name | -n ] service'
+  while [ $# -gt 0 ] ; do
+    case $1 in
+    -n | --name) field=2 ;;
+    -*) echo "$usage" ; exit 2 ;;
+    *) break ;;
+    esac
+    shift
+  done
+  # Add '-2' at the end of the patterns so we find only `stone`, not
+  # `stone-worker`
+  gr-list-instances "${1}" | awk -F'|' "/${1}-2/"' { value=$'"${field}"';gsub(/ /,"",value); print value ; exit }'
+}
+
+ec2-sh() {
+  destination="${1}"
+  shift
+  ssh $(ec2-ip "$destination") "${@}"
+}
