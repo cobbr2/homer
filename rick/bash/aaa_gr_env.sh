7f1c7292a9abc920bbc9364c569b236b8011dea1
Rick Cobb <rick@grandrounds.com>
2020-12-23 15:35:44 -0800

    reflect bootstrap changes to GR conventions, but do not override old stuff yet

--- rick/bash/aaa_gr_env.sh
+++ rick/bash/aaa_gr_env.sh
@@ -1,3 +1,17 @@
-export GR_USERNAME=rick
+export INITIALS=rec
+export GR_GPGKEY_ADDRESS=rick@grnds.com
+
 export GR_HOME=~
+
+# More correct might be:
+export GR_USERNAME=rick.cobb
+export GR_ABBREV_USERNAME=rick
+# But what I had was just:
+export GR_USERNAME=rick
+unset GR_ABBREV_USERNAME
+
+export KEY_SUFFIX=grandrounds.com
+export GIT_ORG=consultingmd
+
+# And I always had:
 export GR_HOST_IP=$(~/bin/lan_ip_address)
