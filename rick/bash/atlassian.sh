ae72cc30f83240a65a2c4c84e20febbdf0f17a35
Rick Cobb <rick.cobb@grandrounds.com>
2022-08-23 11:24:45 -0700

    convenient search functions for jira & confluence

---
+++ rick/bash/atlassian.sh
@@ -0,0 +1,7 @@
+jira() {
+    open https://grandrounds.atlassian.net/secure/QuickSearch.jspa\?searchString\="$1"
+}
+
+conf() {
+    open https://grandrounds.atlassian.net/wiki/dosearchsite.action\?queryString\="$1"
+}
