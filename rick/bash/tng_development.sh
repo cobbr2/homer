09065bb08b99fd36e4139578a887345323785e2c
Rick Cobb <rick.cobb@grandrounds.com>
2023-05-05 14:57:31 -0700

    have ltng use real backend (with warning) if local is not started

--- rick/bash/tng_development.sh
+++ rick/bash/tng_development.sh
@@ -6,7 +6,14 @@ ltng ()
       bin="$cli";
   fi;
 
-  "$bin" --server http://localhost:8000 --timeout 120 "$@"
+  local server=''
+  if nc -w 0 localhost 8000 ; then
+    server="--server http://localhost:8000"
+  else
+    echo "using ${AWS_ENVIRONMENT} server" 1>&2
+  fi
+
+  "$bin" ${server} --timeout 120 "$@"
 }
 
 export IMAGE_BUILDER_ROOT="${IH_HOME}/image-builder"
