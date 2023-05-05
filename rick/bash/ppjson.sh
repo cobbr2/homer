8a780550af114bf2175f36f2dc7ca7aa3dc65546
Rick Cobb <rick.cobb@grandrounds.com>
2023-05-05 14:59:10 -0700

    add json2yaml

--- rick/bash/ppjson.sh
+++ rick/bash/ppjson.sh
@@ -1,11 +1,16 @@
 ppjson ()
 {
-    cat ${1:- --} | ruby -rjson -rpp -e "pp JSON.parse(STDIN.read)"
+  cat ${1:- --} | ruby -rjson -rpp -e "pp JSON.parse(STDIN.read)"
 }
 
 splitandppjson ()
 {
-    cat ${1:- --} | ruby -rjson -rpp -e "pp JSON.parse(STDIN.read)"
+  cat ${1:- --} | ruby -rjson -rpp -e "pp JSON.parse(STDIN.read)"
 }
 
 # For terraform, use `landscape` viz `tf plan | landscape`
+
+json2yaml()
+{
+  cat ${1:- --} | ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(STDIN.read))'
+}
