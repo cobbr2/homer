e9bb2f5e75d05e570fdeae4d7bc750da9e7b67f6
Rick Cobb <rick.cobb@grandrounds.com>
2023-05-05 14:58:30 -0700

    take variable settings from pagerduty alerts in as shell vars

---
+++ rick/bash/kore.sh
@@ -0,0 +1,46 @@
+# Idea is that you can copy from your PD alert,
+# and get local shell variables for the label values.
+#
+# so if you had copied from PagerDuty the strings:
+#
+#  Labels:
+#   - alertname = Nodes Not Ready
+#   - grafana_folder = TNG
+#   - node = ip-10-191-204-79.ec2.internal
+#   - tng_environment = production
+#   - tng_region = us-east-1
+#  Annotations:
+#
+# and you pasted that in like:
+# $ pd-variables <<FOO
+# <paste>
+# FOO
+#
+# you would immediately have as variables
+# $ set
+# alertname="Nodes Not Ready"
+# grafana_folder="TNG"
+# ...
+#
+# Given this, you can then automate runbooks like the ones for Nodes Not Ready
+
+pd_labels_to_bash_assignments() {
+  sed -n '
+    /^ *- / {
+     s/^ *- //
+     s/ = /="/
+     s/$/"/
+     p
+   }
+  '
+}
+
+pd_variables() {
+echo "Paste your buffer from PagerDuty here and then use CTRL-D"
+
+eval $(pd_labels_to_bash_assignments)
+}
+
+instance_from_node() {
+  eval $($GR_HOME/kore/bin/list-nodes | grep "/^$node/" | tee /dev/stderr | awk '/^'"${node}"'/ { print "instance=" $5 }')
+}
